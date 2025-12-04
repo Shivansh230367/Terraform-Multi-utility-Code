5fd590d5e0b9febc8153c220fd34aad501fcb367
Shivansh Chaurasia <146476709+Shivansh230367@users.noreply.github.com>
2025-12-04 17:25:17 +0530

    Add files via upload

---
+++ task1/task_1.tf
@@ -0,0 +1,126 @@
+// question1/main.tf
+// CHANGE: Replace highlighted tokens below before running
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+variable "aws_region" {
+  type    = string
+  default = "us-east-1" // <span style="color:red">REPLACE_ME_REGION</span>
+}
+
+variable "resource_prefix" {
+  type    = string
+  default = "Shivansh_Chaurasia" // <span style="color:red">REPLACE_ME_PREFIX</span>
+}
+
+# VPC
+resource "aws_vpc" "this" {
+  cidr_block = "10.0.0.0/16"
+  tags       = { Name = "${var.resource_prefix}_vpc" }
+}
+
+# Internet Gateway
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.this.id
+  tags   = { Name = "${var.resource_prefix}_igw" }
+}
+
+data "aws_availability_zones" "available" {}
+
+# Public subnets
+resource "aws_subnet" "public_a" {
+  vpc_id                  = aws_vpc.this.id
+  cidr_block              = "10.0.1.0/24" # PUBLIC A
+  availability_zone       = data.aws_availability_zones.available.names[0]
+  map_public_ip_on_launch = true
+  tags                    = { Name = "${var.resource_prefix}_public_a" }
+}
+
+resource "aws_subnet" "public_b" {
+  vpc_id                  = aws_vpc.this.id
+  cidr_block              = "10.0.2.0/24" # PUBLIC B
+  availability_zone       = data.aws_availability_zones.available.names[1]
+  map_public_ip_on_launch = true
+  tags                    = { Name = "${var.resource_prefix}_public_b" }
+}
+
+# Private subnets
+resource "aws_subnet" "private_a" {
+  vpc_id            = aws_vpc.this.id
+  cidr_block        = "10.0.101.0/24" # PRIVATE A
+  availability_zone = data.aws_availability_zones.available.names[0]
+  tags              = { Name = "${var.resource_prefix}_private_a" }
+}
+
+resource "aws_subnet" "private_b" {
+  vpc_id            = aws_vpc.this.id
+  cidr_block        = "10.0.102.0/24" # PRIVATE B
+  availability_zone = data.aws_availability_zones.available.names[1]
+  tags              = { Name = "${var.resource_prefix}_private_b" }
+}
+
+# Public route table -> IGW
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.this.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+  tags = { Name = "${var.resource_prefix}_public_rt" }
+}
+
+resource "aws_route_table_association" "public_a" {
+  subnet_id      = aws_subnet.public_a.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "public_b" {
+  subnet_id      = aws_subnet.public_b.id
+  route_table_id = aws_route_table.public.id
+}
+
+# NAT Gateway (NOTE: NAT Gateway costs money)
+resource "aws_eip" "nat" {
+  vpc  = true
+  tags = { Name = "${var.resource_prefix}_nat_eip" }
+}
+
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public_a.id
+  tags          = { Name = "${var.resource_prefix}_nat" }
+}
+
+# Private route table -> NAT GW
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.this.id
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat.id
+  }
+  tags = { Name = "${var.resource_prefix}_private_rt" }
+}
+
+resource "aws_route_table_association" "private_a" {
+  subnet_id      = aws_subnet.private_a.id
+  route_table_id = aws_route_table.private.id
+}
+
+resource "aws_route_table_association" "private_b" {
+  subnet_id      = aws_subnet.private_b.id
+  route_table_id = aws_route_table.private.id
+}
+
+output "vpc_id" {
+  value = aws_vpc.this.id
+}
